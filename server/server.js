ef8b002c1b88d1c4252e0c92a390d92436df4376
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-10-10 12:34:50 +0400

    preparing for further development

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import app from './app.js'
 import mongoose from 'mongoose'
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-mongoose.connect(mongoURL).then((res) => {
+mongoose.connect(process.env.MONGODB_CONN_STR).then((res) => {
     console.log('db connection successfull')
 })
 
